197f01d1d49bef84f8aa6f41236073ce165b704e
kidist tadesse <187760549+Kidtd12@users.noreply.github.com>
2025-11-06 10:37:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleDark = document.getElementById("toggleDark");
   const voiceBtn = document.getElementById("voiceBtn");
 
-  const voiceBtn = document.getElementById("voiceBtn");
   
 
   // Theme handler
@@ -157,6 +156,7 @@ async function translateToOromo(text) {
   const data = await res.json();
   return data.data.translations[0].translatedText;
 }
+
 
 
 
